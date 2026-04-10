e55d2ada46fb0a248bd8e9ebe620257349883360
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-04-11 04:05:23 +0500

    final fixed the bloody shadcn

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Button } from "../../components/ui/button"
+import Header from '../../components/custom/Header'
+
 const page = () => {
   return (
     <div>
+     
       <h1 className="font-manrope text-2xl">
-  Hello World
-</h1>
-      <Button >Click Me</Button>
-
+        Hello World
+      </h1>
+      <Button>Click Me</Button>
     </div>
   )
 }
---
+++ src/components/custom/tenant-select.tsx
@@ -0,0 +1,34 @@
+'use client';
+import React from 'react';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
+import { Tenant } from '@/lib/types';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+const TenantSelect = ({ restaurants }: { restaurants: { data: Tenant[] } }) => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const handleValueChange = (value: string) => {
+        router.push(`/?restaurantId=${value}`);
+    };
+    return (
+        <Select
+            onValueChange={handleValueChange}
+            defaultValue={searchParams.get('restaurantId') || ''}>
+            <SelectTrigger className="w-[180px] focus:ring-0">
+                <SelectValue placeholder="Select Restaurant" />
+            </SelectTrigger>
+            <SelectContent>
+                {restaurants.data.map((restaurant) => {
+                    return (
+                        <SelectItem key={restaurant.id} value={String(restaurant.id)}>
+                            {restaurant.name}
+                        </SelectItem>
+                    );
+                })}
+            </SelectContent>
+        </Select>
+    );
+};
+
+export default TenantSelect;
