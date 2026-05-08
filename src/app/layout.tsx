97c0a1b3be6a40cf5a9c5f9b03f12c34763c2fe3
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-05-08 23:40:25 +0500

    fetching custmer data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Header from '../components/custom/Header';
 import StoreProvider from './StoreProvider';
 import { Toaster } from "../components/ui/sonner";
 import Refresher from '../components/custom/refresher';
-
+import QueryProvider from './QueryProvider';
 const manrope = Manrope({ subsets: ['latin'], variable: '--font-manrope' });
 
 export const metadata: Metadata = {
@@ -27,11 +27,13 @@ export default function RootLayout({
                         'min-h-screen bg-background font-manrope antialiased',
                         manrope.variable
                     )}>
-                    <Refresher>
-                        <Header />
-                        <main>{children}</main>
-                        <Toaster />
-                    </Refresher>
+                   <QueryProvider>
+                        <Refresher>
+                            <Header />
+                            <main>{children}</main>
+                            <Toaster />
+                        </Refresher>
+                    </QueryProvider>
                 </body>
             </StoreProvider>
         </html>
