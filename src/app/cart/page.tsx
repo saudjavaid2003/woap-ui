9b8b29f77cb701eb3a2549611b9429c15369d612
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-05-30 10:56:39 +0500

    deploying

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,6 @@
+// cart/page.tsx
 'use client';
-import React from 'react';
+import React, { Suspense } from 'react';
 import CartItems from './cartItems/cartItems';
 
 const CartPage = () => {
@@ -8,7 +9,9 @@ const CartPage = () => {
             <div className="container mx-auto py-6">
                 <h1 className="text-lg font-bold">Shopping cart</h1>
                 <div className="bg-white rounded-lg p-6 mt-6">
-                    <CartItems />
+                    <Suspense fallback={<div>Loading cart...</div>}>
+                        <CartItems />
+                    </Suspense>
                 </div>
             </div>
         </section>
